d4cedc407d91d030488c9e0fb9cab482884c1efb
MonicaAcha2103 <monicaacha2103@gmail.com>
2023-03-27 23:58:25 +0530

    Add darkmode support

---
+++ components/themetoggle.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+
+const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme("light");
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const toggleDarkMode = (checked) => {
+    if (theme === "light") setTheme("dark");
+    else setTheme("light");
+  };
+
+  if (!mounted) {
+    return null;
+  }
+  return (
+    // <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+    //   <option value="system">System</option>
+    //   <option value="dark">Dark</option>
+    //   <option value="light">Light</option>
+    // </select>
+    // <DarkModeSwitch
+    //   style={{ marginBottom: "2rem" }}
+    //   checked={theme === "dark"}
+    //   onChange={toggleDarkMode}
+    //   size={120}
+    //   moonColor="blue"
+    // />
+    <DarkModeToggle
+      onChange={toggleDarkMode}
+      checked={theme === "dark"}
+      size={80}
+    />
+  );
+};
+
+export default ThemeToggle;
